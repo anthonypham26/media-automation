412c6d57479fe83c85bce31978bdce86caa1d38b
anthonypham26 <anthonypham.c22@gmail.com>
2024-10-03 17:11:23 -0500

    Split into video and Audio

--- scripts/clipper.py
+++ scripts/clipper.py
@@ -1,36 +1,31 @@
 import os
 from pytubefix import YouTube
 
-
+#input: youtube url, outputs into videos/ and audios/
 def download_video(url):
     yt = YouTube(url)
-    yt.streams.filter(res = "1080p").first().download('videos/')
+    video_file = yt.streams.filter(only_video= True, file_extension = 'mp4').order_by('resolution').desc().first()
+    audio_file = yt.streams.filter(only_audio=True).first()
+    video_file.download('videos/')
+    audio_file.download(output_path= 'audios/', mp3 = True)
     print(f'Title: {yt.title}')
 
 download_video('https://youtu.be/PugKQZHPut8?si=V2EtRZqJ9yFkf3BU')
 
 
+def crop_vertical():
 
+    return 0
 
+def generate_subtitles(audio_file):
+    # model = whisper.load_model("base.en")
 
+    return 0
 
+def split_video_into_minute():
 
+    return 0
 
+def run_clipper():
 
-# def crop_vertical():
-
-#     return 0
-
-# import whisper
-# def generate_subtitles(audio_file):
-#     model = whisper.load_model("base.en")
-
-#     return 0
-
-# def split_video_into_minute():
-
-#     return 0
-
-# def run_clipper():
-
-#     return 0
+    return 0
