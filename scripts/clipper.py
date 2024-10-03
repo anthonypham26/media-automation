d439280d032a657b9b5614b20b612cad1aba2fe1
anthonypham26 <anthonypham.c22@gmail.com>
2024-10-03 13:43:37 -0500

    Setup W

---
+++ scripts/clipper.py
@@ -0,0 +1,23 @@
+
+
+def download_video():
+
+    return 0
+
+def crop_vertical():
+
+    return 0
+
+import whisper
+def add_subtitles(audio_file):
+    model = whisper.load_model("base.en")
+    
+    return 0
+
+def split_video_into_minute():
+
+    return 0
+
+def run_clipper():
+
+    return 0
