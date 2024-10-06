caa1dede23ab4c444ebe4728907e27ade66d6057
anthonypham26 <anthonypham.c22@gmail.com>
2024-10-06 16:06:12 -0500

    implemented crop_vert

--- scripts/clipper.py
+++ scripts/clipper.py
@@ -1,21 +1,34 @@
 import os
 from pytubefix import YouTube
+import ffmpeg
+import moviepy.editor as mpy
+from moviepy.video.fx.all import crop
 
-#input: youtube url, outputs into videos/ and audios/
+# input: youtube url, outputs into videos/ and audios/
 def download_video(url):
     yt = YouTube(url)
+    print(f"Downloading {yt.title} from Youtube")
     video_file = yt.streams.filter(only_video= True, file_extension = 'mp4').order_by('resolution').desc().first()
     audio_file = yt.streams.filter(only_audio=True).first()
     video_file.download('videos/')
     audio_file.download(output_path= 'audios/', mp3 = True)
-    print(f'Title: {yt.title}')
+    path = 'videos/' + yt.title + '.mp4'
+    print(f"Path: {path}")
+    return path
 
-download_video('https://youtu.be/PugKQZHPut8?si=V2EtRZqJ9yFkf3BU')
+def crop_vertical(path):
+    print(f"Cropping Video...")
+    clip = mpy.VideoFileClip(path)
 
+    (original_w,original_h) = clip.size
+    new_w = int((9/16) * original_h )
 
-def crop_vertical():
+    clip_cropped = crop(clip, width = new_w, height = original_h, x_center = int(original_w/2), y_center = int(original_h/2) )
+    new_path = path.replace('.mp4', '_cropped_vert.mp4')    
+    clip_cropped.write_videofile(new_path)
 
-    return 0
+path = download_video('https://youtu.be/2dI9ql8KSH4?si=RuSB8HKz0auCCIXd')
+crop_vertical(path)
 
 def generate_subtitles(audio_file):
     # model = whisper.load_model("base.en")
